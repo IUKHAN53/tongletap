ee1644737f2e51be86b36527864ee80bf5479eb0
Irfan Ullah <iukhan53@gmail.com>
2023-09-21 13:14:11 +0500

    fixes
    Signed-off-by: Irfan Ullah <iukhan53@gmail.com>

--- app/Models/HealthStat.php
+++ app/Models/HealthStat.php
@@ -96,21 +96,21 @@ class HealthStat extends Model
         return [
             'depression' => [
                 'status' => $stat->depressionStatus ?? '',
-                'percentage' => number_format($stat->depressionPercentage ?? 0, 2),
+                'percentage' => number_format($stat->depressionPercentage ?? 0),
                 'score' => $stat->depressionScore ?? 0,
                 'icon' => $depressionIcon,
                 'color' => $depressionColor,
             ],
             'anxiety' => [
                 'status' => $stat->anxietyStatus ?? '',
-                'percentage' => number_format($stat->anxietyPercentage ?? 0, 2),
+                'percentage' => number_format($stat->anxietyPercentage ?? 0),
                 'score' => $stat->anxietyScore ?? 0,
                 'icon' => $anxietyIcon,
                 'color' => $anxietyColor,
             ],
             'stress' => [
                 'status' => $stat->stressStatus ?? '',
-                'percentage' => number_format($stat->stressPercentage ?? 0, 2),
+                'percentage' => number_format($stat->stressPercentage ?? 0),
                 'score' => min($stat->stressScore ?? 0, 20),
                 'icon' => $stressIcon,
                 'color' => $stressColor,
